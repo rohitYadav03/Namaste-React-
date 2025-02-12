9ddee4d5e4ed7be51fdf9d50a5de58fc2e311d29
Rohit Yadav <rohityadav85801@gmail.com>
2025-02-12 17:34:33 +0000

    Episode 2

---
+++ Episode 2/dist/index.7c0ccee6.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello World from React 12");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
